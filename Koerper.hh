5c55ce862cbfc2803cc7069ea5757bd5f5f4a73e
Gamingtics <102976891+Gamingtics@users.noreply.github.com>
2022-09-02 12:03:23 +0000

    ÜB7 done

---
+++ Koerper.hh
@@ -0,0 +1,18 @@
+#ifndef KOERPER_HH
+#define KOERPER_HH
+
+#include "Vektor.hh"
+
+class Koerper {
+public:
+
+virtual Vektor punkt(){
+};
+
+virtual std :: string name () { return " Koerper ";}
+
+private:
+
+};
+
+#endif
